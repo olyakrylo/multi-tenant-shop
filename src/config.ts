70f1b20379c7b67429eb1552ac006e42d7f34484
olyakrylo <olyakrylo24@gmail.com>
2020-12-07 02:09:53 +0300

    add deploy version

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const config = {
   name: "AF",
-  token: "ed724c6c1aa41ac577ab797596cde947d59c6cfa",
-  url: "http://192.168.1.69:8000/shop/",
+  token: "e46d15ec7da1a9d48a325c8dd82d8d58b9d73245",
+  url: "https://multitenant-backend.herokuapp.com/shop/",
   login_time: 30, // minutes
   cart_time: 24, // hours
   languages: [
